3d70cb76feb7682b20e35a9b379e98588961b83e
cMatheusBRibeiro <mbribeiropr@gmail.com>
2021-03-26 01:42:00 +0000

    wip

--- spring-boot-app/src/test/java/br/com/blog/springbootapp/SpringBootAppApplicationTests.java
+++ spring-boot-app/src/test/java/br/com/blog/springbootapp/SpringBootAppApplicationTests.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.blog.springbootapp.entity.Usuario;
 import br.com.blog.springbootapp.repository.UsuarioRepository;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class SpringBootAppApplicationTests {
 
     @Autowired
@@ -21,11 +25,19 @@ class SpringBootAppApplicationTests {
     @Test
     void testeInsercao() {
 
+        // Instanciando a classe usuário
         Usuario usuario = new Usuario();
-        usuario.setNome("Matheus");
+
+        // Setando valores no objeto
+        usuario.setNome("Jaiminho");
+        usuario.setLogin("jaime");
         usuario.setSenha("SenhaF0rte");
-        usuario.setLogin("math");
+        usuario.setAtivo(1);
+
+        // Salvando no banco de dados
         usuarioRepo.save(usuario);
+
+        // Validando se o ID não é nulo
         assertNotNull(usuario.getId());
     }
 
