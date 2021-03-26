b2cb92625d6054569d61e0f7bf1b2d051bc85f63
cMatheusBRibeiro <mbribeiropr@gmail.com>
2021-03-26 01:24:32 +0000

    wip

--- spring-boot-app/src/main/java/br/com/blog/springbootapp/entity/Usuario.java
+++ spring-boot-app/src/main/java/br/com/blog/springbootapp/entity/Usuario.java
@@ -1,19 +1,34 @@
 package br.com.blog.springbootapp.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-
+@Entity
+@Table(name = "USUARIO")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USR_ID")
     private Integer id;
 
+    @Column(name = "ADMIN_ID")
     private Integer admin;
 
+    @Column(name = "USR_LOGIN")
     private String login;
 
+    @Column(name = "USR_SENHA")
     private String senha;
 
+    @Column(name = "USR_NOME")
     private String nome;
 
+    @Column(name = "USR_ATIVO")
     private Integer ativo;
 
     
--- spring-boot-app/src/test/java/br/com/blog/springbootapp/SpringBootAppApplicationTests.java
+++ spring-boot-app/src/test/java/br/com/blog/springbootapp/SpringBootAppApplicationTests.java
@@ -1,13 +1,32 @@
 package br.com.blog.springbootapp;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.blog.springbootapp.entity.Usuario;
+import br.com.blog.springbootapp.repository.UsuarioRepository;
 
 @SpringBootTest
 class SpringBootAppApplicationTests {
 
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+
 	@Test
 	void contextLoads() {
-	}
+    }
+    
+    @Test
+    void testeInsercao() {
+
+        Usuario usuario = new Usuario();
+        usuario.setNome("Matheus");
+        usuario.setSenha("SenhaF0rte");
+        usuario.setLogin("math");
+        usuarioRepo.save(usuario);
+        assertNotNull(usuario.getId());
+    }
 
 }
